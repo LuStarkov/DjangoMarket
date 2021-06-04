b22728ed9992b0bfb0c98ddfe6f07df8b2e4d167
AleksandrBusov <78384347+AleksandrBusov@users.noreply.github.com>
2021-06-04 16:58:03 +0500

    Добавление товара в корзину

--- DjangoMarket/mainapp/urls.py
+++ DjangoMarket/mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from.views import test_view, ProductDetailView, CartView
+from.views import test_view, ProductDetailView, CartView, AddToCartView
 
 urlpatterns = [
     path('', test_view, name='base'),
     path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
-    path('cart/', CartView.as_view(), name='cart')
+    path('cart/', CartView.as_view(), name='cart'),
+    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart')
 ]
--- DjangoMarket/mainapp/views.py
+++ DjangoMarket/mainapp/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
 from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
 
 
 def test_view(request):
+    customer = Customer.objects.get(user=request.user)
+    cart = Cart.objects.get(owner=customer)
     categories = Category.objects.get_categories()
     products = LatestProducts.objects.get_products_for_main_page(
         'notebook', 'smartphone', with_respect_to='smartphone'
     )
     context = {
         'categories': categories,
-        'products': products
+        'products': products,
+        'cart': cart
     }
     return render(request, 'base.html', context)
 
@@ -30,6 +35,25 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'),  kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id
+        )
+        if created:
+            cart.product.add(cart_product)
+        return HttpResponseRedirect('/cart')
 
 class CartView(View):
 
