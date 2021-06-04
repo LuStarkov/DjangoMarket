dc507f3a8d2e2a063e9002edc38894734329ec6d
AleksandrBusov <78384347+AleksandrBusov@users.noreply.github.com>
2021-06-04 16:41:19 +0500

    Добавление корзины

--- DjangoMarket/mainapp/views.py
+++ DjangoMarket/mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category, LatestProducts
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
 
 
 def test_view(request):
@@ -30,3 +30,16 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
+
+class CartView(View):
+
+    def get(self, request, *args,  **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
+
